738adedccb6c3157b06638a7430ee403b1134755
kzhrjp <oneandmore@gmail.com>
2017-12-22 01:45:14 +0900

    delete unexpcted char

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         ],
 
         'circle_test' => [
-	        'driver' => 'mysql',C
+	        'driver' => 'mysql',
 	        'host' => '127.0.0.1',
 	        'port' => '3306',
 	        'database' => 'circle_test',
